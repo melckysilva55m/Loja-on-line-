1e78f8f7888c18f86422414519280037305909f6
melckysilva55m <melquesedec133@gmail.com>
2024-12-25 06:35:42 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Inicializa a biblioteca AOS (Animate on Scroll)
-AOS.init({
-    duration: 1000, // Duração da animação
-    easing: 'ease-in-out', // Efeito de transição
-    once: true, // Anima uma única vez
-});
