da1afc0f7018793ee557f761261169ea3a93d5db
melckysilva55m <melquesedec133@gmail.com>
2024-12-25 05:54:48 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Inicializa a biblioteca AOS (Animate on Scroll)
+AOS.init({
+    duration: 1000, // Duração da animação
+    easing: 'ease-in-out', // Efeito de transição
+    once: true, // Anima uma única vez
+});
